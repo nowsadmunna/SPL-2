6070c293072b75a8f46131dd58ffc3b13910480c
abulkalamazad1425 <bsse1425@iit.du.ac.bd>
2025-01-09 13:02:57 +0600

    changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      About shahidullah hall mess.shahidullah hall mess management system
+      About shahidullah hall mess.shahidullah hall mess management system in dhaka university.
     </div>
   )
 }
